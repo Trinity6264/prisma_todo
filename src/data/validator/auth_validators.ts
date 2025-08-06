972e1ec7e1de780d532432b81a7150bdc5caec2d
Trinity6264 <amoahtnt6@gmail.com>
2025-08-06 01:43:30 +0000

    Add validation rules for user registration and login requests

---
+++ src/data/validator/auth_validators.ts
@@ -0,0 +1,27 @@
+import { body } from 'express-validator';
+
+export const registerValidationRules = [
+    body('email')
+        .isEmail()
+        .withMessage('Email is not valid'),
+
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+
+    body('username')
+        .notEmpty()
+        .withMessage('Username is required').isLength({ min: 3 })
+        .withMessage('Username must be at least 3 characters long'),
+];
+
+export const loginValidationRules = [
+    body('email')
+        .isEmail()
+        .withMessage('Email is not valid'),
+
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+
+];
---
+++ src/data/validator/validation_request.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import AsyncWrapper from '../../helper/async_wrapper';
+import BadRequestError from '../../errors/bad_request_error';
+
+export const validateRequest = AsyncWrapper(async (req: Request, res: Response, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const message = errors.array()[0]?.msg;
+        throw new BadRequestError(message);
+    }
+    next();
+});
+
+
